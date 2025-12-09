e1e0132a7fc17a2d4d0c57eec061d55c1258a82c
Devin Thang <luuthang7803@gmail.com>
2025-12-09 15:22:05 +0800

    add ddim done

--- Training/pipeline/pipeline.py
+++ Training/pipeline/pipeline.py
@@ -333,6 +333,7 @@ class FlowControlNetPipeline(DiffusionPipeline):
 
         # 4. Encode input image using VAE
         image = self.image_processor.preprocess(image, height=height, width=width)
+        image = image.to(device=device)
         noise = randn_tensor(image.shape, generator=generator, device=image.device, dtype=image.dtype)
         image = image + noise_aug_strength * noise
 
